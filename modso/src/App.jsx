483f2dc308079a1adb71d41946002bb59a1bd02f
romeroac <romeroa@correo.unimet.edu.ve>
2023-10-23 13:40:20 -0400

    init

--- modso/src/App.jsx
+++ modso/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Header from "./components/Header";
+import Categories from "./components/landing/Categories";
+import Contact from "./components/landing/Contact";
+import Hero from "./components/landing/Hero";
+import Brands from "./components/landing/Brands";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Hero />
+      <Brands />
+      <Categories />
+      <Contact />
     </>
-  )
+  );
 }
-
-export default App
