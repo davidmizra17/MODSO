3aa787246cce846c91de84a82c721c8c07d0e222
GuilleSanfuentes <65252200+GuilleSanfuentes@users.noreply.github.com>
2023-11-29 18:01:12 -0400

    Update NotFoundPage.jsx

--- modso/src/pages/NotFoundPage.jsx
+++ modso/src/pages/NotFoundPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 // Desc: 404 Page Not Found
 // Manejador de errores
 // cuando no existe el URL en el router
@@ -6,6 +7,15 @@ const NotFoundPage = () => {
         <div>
             <h1>404 Page Not Found</h1>
             <p>The page you are looking for does not exist.</p>
+        
+            <Link to="/">
+            <button className="bg-primary-500 px-6 py-4 rounded-full
+            text-2xl text-white uppercase transition
+             hover:bg-primary-400 hover:scale-95
+             active:bg-primary-700"
+            >
+                Para volver a la pagina de inicio, haga click aqui
+            </button></Link>
         </div>
     );
 };
